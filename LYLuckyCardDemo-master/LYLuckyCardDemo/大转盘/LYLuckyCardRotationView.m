d49782e2810ad0f2f6e65eba0c433012b9ee9f45
shanyou2016 <631797337@qq.com>
2020-05-18 20:17:13 +0800

    修复中奖后没有指到中奖位置

--- LYLuckyCardDemo-master/LYLuckyCardDemo/大转盘/LYLuckyCardRotationView.m
+++ LYLuckyCardDemo-master/LYLuckyCardDemo/大转盘/LYLuckyCardRotationView.m
@@ -201,7 +201,7 @@
         NSLog(@"奖品id = %@", prizeId );
         NSLog(@"prizeName= %@", [data objectForKey:@"prizeName"] );
         NSLog(@"prizeIcon= %@", [data objectForKey:@"prizeIcon"] );
-        [self.canRotationView.layer removeAllAnimations];
+      
         if (self.angle != -1) {
              [self animationWinning];
         }
